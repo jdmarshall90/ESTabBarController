de8a564c2effb3ca8e188db8ccd897a15a0aed6c
Justin Marshall <jdmarshall90@comcast.net>
2017-01-20 18:58:13 -0600

    adding a project to build the lib as a framework

---
+++ ESTabBarController/ESTabBarController/ESTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  ESTabBarController.h
+//  ESTabBarController
+//
+//  Created by Justin Marshall on 1/20/17.
+//  Copyright © 2017 Justin Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESTabBarController.
+FOUNDATION_EXPORT double ESTabBarControllerVersionNumber;
+
+//! Project version string for ESTabBarController.
+FOUNDATION_EXPORT const unsigned char ESTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESTabBarController/PublicHeader.h>
+
+
